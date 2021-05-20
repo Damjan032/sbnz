736fdf51b8cbbd26432cdbb4fad9ecf7513b0d4a
Stefan <stefan.kandic@yahoo.com>
2021-05-20 11:33:28 +0200

    drools works now!

--- AdvertisingSystem/src/main/java/sbnz/advertisingsystem/DroolsTest.java
+++ AdvertisingSystem/src/main/java/sbnz/advertisingsystem/DroolsTest.java
@@ -7,15 +7,47 @@ import org.kie.api.runtime.KieSession;
 public class DroolsTest {
 
     public static void main(String[] args) {
-        System.out.println("I got here");
-        System.out.println("");
         try {
+            // load up the knowledge base
             KieServices ks = KieServices.Factory.get();
             KieContainer kContainer = ks.getKieClasspathContainer();
             KieSession kSession = kContainer.newKieSession("ksession-rules");
 
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
     }
 }
