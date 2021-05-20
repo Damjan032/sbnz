5936353ab1cfd04a126163a55b618ae9da1ecf9a
Damjan Manojlović <damjan0032@gmail.com>
2021-05-20 15:36:40 +0200

    intellij and drool working together

---
+++ adsys/src/main/resources/data-postgres.sql
@@ -0,0 +1,4 @@
+/*------------------------ROLE-----------------------------------------*/
+INSERT INTO authority (name) VALUES ('ROLE_READER');
+INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
+/*------------------------ROLE-----------------------------------------*/
---
+++ adsys/src/test/java/com/sbnz/ibar/TestAll.java
@@ -0,0 +1,5 @@
+package com.sbnz.adsys;
+
+public class TestAll {
+
+}
